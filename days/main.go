9a0fd29b85e6159a24963f1fff8247ea14195a62
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2023-12-06 13:36:46 +0000

    Day 2, Part 1: Could definitely do this with just one loop through the input

--- days/main.go
+++ days/main.go
@@ -14,18 +14,23 @@ func One(part int) {
 	}
 	if part == 1 {
 		sum := one.RunPartOne(string(b))
-		fmt.Printf("Sum (Part 1): %d\n", sum)
+		fmt.Printf("Day 1, Part 1\nSum: %d\n", sum)
 		return
 	}
 
 	if part == 2 {
 		sum := one.RunPartTwo(string(b))
-		fmt.Printf("Sum (Part 2): %d\n", sum)
+		fmt.Printf("Day 1, Part 2\nSum: %d\n", sum)
 	}
 }
 
 func Two(part int) {
+	b, err := os.ReadFile("./days/two/input.txt")
+	if err != nil {
+		panic("Failed to open file")
+	}
 	if part == 1 {
-		two.RunPartOne()
+		sum := two.RunPartOne(string(b))
+		fmt.Printf("Day 2, Part 1\nSum: %d\n", sum)
 	}
 }
--- days/two/main.go
+++ days/two/main.go
@@ -1,7 +1,62 @@
 package two
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-func RunPartOne() {
-	fmt.Printf("Hi")
+func getGameId(game string) int {
+	firstColonIdx := strings.Index(game, ":")
+	gameIdString := game[5:firstColonIdx]
+	gameId, err := strconv.Atoi(gameIdString)
+	if err != nil {
+		fmt.Printf("Could not find valid game ID integer")
+		return 0
+	}
+	return gameId
+}
+
+var maxCubes = map[string]int{
+	"red":   12,
+	"green": 13,
+	"blue":  14,
+}
+
+func extractSets(game string) []string {
+	firstColonIdx := strings.Index(game, ":")
+	setsString := game[firstColonIdx+2:]
+	return strings.Split(setsString, "; ")
+}
+
+func isGameValid(game string) bool {
+	sets := extractSets(game)
+	for _, set := range sets {
+		colors := strings.Split(set, ", ")
+		for _, color := range colors {
+			split := strings.Split(color, " ")
+			count, err := strconv.Atoi(split[0])
+			if err != nil {
+				fmt.Printf("Could not find count of blocks")
+				continue
+			}
+			if count > maxCubes[split[1]] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func RunPartOne(input string) int {
+	games := strings.Split(input, "\n")
+	var sum int
+	for _, game := range games {
+		game = strings.TrimSpace(game)
+		gameId := getGameId(game)
+		if isGameValid(game) {
+			sum += gameId
+		}
+	}
+	return sum
 }
