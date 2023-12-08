0d41e2086361e1b124be7053ae74e5f051966d59
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2023-12-08 13:50:40 +0000

    Test to calculate card score

--- days/4/main.go
+++ days/4/main.go
@@ -1,9 +1,16 @@
 package four
 
+import "fmt"
+
+func CalculateCardScore(card string) int {
+	return 0
+}
+
 func RunPartOne(input string) int {
 	return 0
 }
 
 func RunPartTwo(input string) int {
+	fmt.Printf("Implement me!\n")
 	return 0
 }
--- days/4/main_test.go
+++ days/4/main_test.go
@@ -1,7 +1,9 @@
 package four_test
 
 import (
+	"fmt"
 	"jayseejay/advent-of-code-23/days/4"
+	"strings"
 	"testing"
 )
 
@@ -12,10 +14,28 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
 
+var cardScores = [6]int{8, 2, 2, 1, 0, 0}
+
 func TestRunPartOne(t *testing.T) {
 	got := four.RunPartOne(example)
 
 	if got != 13 {
 		t.Errorf("RunPartOne(Card 1: 41...) = %d, want 13", got)
+	}
+}
+
+func TestCalculateCardScore(t *testing.T) {
+	cards := strings.Split(example, "\n")
+
+	for i, card := range cards {
+		t.Run(card[:6], func(t *testing.T) {
+			got := four.CalculateCardScore(card)
+			if got != cardScores[i] {
+				t.Errorf("CalculateCardScore(%s) = %d, want %d",
+					card[:6],
+					got,
+					cardScores[i])
+			}
+		})
 	}
 }
